fd6c3d8e52ecb3972705a692ebbc90f8aca8671e
Yehor Levchenko <yehor.levchenko@nure.ua>
2026-09-24 12:06:53 +0300

    perf: optimize prime check loop to square root of n

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ except ValueError:
 if num == 0 or num == 1:
     print(num, "is not a prime number")
 elif num > 1:
-   for i in range(2,num):
+   for i in range(2, int(num**0.5) + 1)::
        if (num % i) == 0:
            print(num,"is not a prime number")
            print(i,"times",num//i,"is",num)
